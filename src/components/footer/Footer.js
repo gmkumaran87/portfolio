a291e379b9f5463737b53307a683523c920f8f6b
Muthukumaran <gmkumaran87@gmail.com>
2022-02-11 13:14:42 +0530

    Added links to Footer icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.scss";
-import { EmailOutlined, Twitter, LinkedIn } from "@mui/icons-material/";
+import { Twitter, LinkedIn, GitHub } from "@mui/icons-material/";
 
 const Footer = () => {
   return (
@@ -8,9 +8,27 @@ const Footer = () => {
       <div className="wrapper">
         <h2>You can find me on</h2>
         <div className="footer-icons">
-          <Twitter className="icon" />
-          <EmailOutlined className="icon" />
-          <LinkedIn className="icon" />
+          <a
+            href="https://twitter.com/gmkumaran87"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Twitter className="icon" />
+          </a>
+          <a
+            href="https://github.com/gmkumaran87"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHub className="icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gmkumaran"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedIn className="icon" />
+          </a>
         </div>
       </div>
     </div>
